cb4127532bea84d0c50cd8baf47785c8b2082235
nikolabuzej <nikola.buzej.mes@gmail.com>
2020-03-09 18:27:45 +0100

    Updates

---
+++ precache-manifest.52aceb776e2e6b171f582f776b8ca54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941085bbd5f3514ad51663b18e1a54b5",
+    "url": "/nikolabuzej/bookSearch.git/index.html"
+  },
+  {
+    "revision": "5b4c16b570bdee4e15f9",
+    "url": "/nikolabuzej/bookSearch.git/static/css/main.f2944620.chunk.css"
+  },
+  {
+    "revision": "91c2cef56364e91134c9",
+    "url": "/nikolabuzej/bookSearch.git/static/js/2.0127c7f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nikolabuzej/bookSearch.git/static/js/2.0127c7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4c16b570bdee4e15f9",
+    "url": "/nikolabuzej/bookSearch.git/static/js/main.ab18bd74.chunk.js"
+  },
+  {
+    "revision": "83ead82a77abfae912e2",
+    "url": "/nikolabuzej/bookSearch.git/static/js/runtime-main.e173c89e.js"
+  }
+]);
